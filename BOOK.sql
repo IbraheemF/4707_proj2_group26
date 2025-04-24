f5eac76bdfd083c32682c023addc2e69e3c4e784
IbraheemF <141586321+IbraheemF@users.noreply.github.com>
2025-04-24 12:45:25 -0500

    Update BOOK.sql

--- BOOK.sql
+++ BOOK.sql
@@ -2,7 +2,7 @@ CREATE TABLE BOOK (
 BookID INT PRIMARY KEY,
 Title VARCHAR(50),
 Genre VARCHAR(50),
-Price FLOAT,
+Price FLOAT(4),
 Stock INT,
 PublisherName VARCHAR(50),
 PublisherContactInfo VARCHAR(50)
