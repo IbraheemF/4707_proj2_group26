97ed6ddfb63ee3a458579b79c789026185cb8905
Caleb Heyden <146133573+calebh99@users.noreply.github.com>
2025-04-24 12:38:23 -0500

    Update PAYMENT.sql

--- PAYMENT.sql
+++ PAYMENT.sql
@@ -1,1 +1,5 @@
-CREATE TABLE PAYMENT ( PaymentID INT PRIMARY KEY, PaymentMethod VARCHAR(50), PaymentDate   ); 
+CREATE TABLE PAYMENT ( 
+  PaymentID INT PRIMARY KEY, 
+  PaymentMethod VARCHAR(50), 
+  PaymentDate VARCHAR(50),  
+  ); 
