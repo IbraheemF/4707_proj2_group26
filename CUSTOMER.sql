6f793e1b84b19f779ed66f4ec7d8d7bab890043c
djcatlin <152668717+djcatlin@users.noreply.github.com>
2025-04-24 12:39:55 -0500

    Update CUSTOMER.sql

--- CUSTOMER.sql
+++ CUSTOMER.sql
@@ -1,1 +1,11 @@
-CREATE TABLE CUSTOMER ( user_id INT PRIMARY KEY, username VARCHAR(50)); 
+CREATE TABLE CUSTOMER (
+  CustomerID INT PRIMARY KEY, 
+  FName VARCHAR(50),
+  LName VARCHAR(50),
+  Email VARCHAR(50),
+  PNumber INT,
+  Street VARCHAR(50),
+  Zip INT,
+  City VARCHAR(50),
+  StateName VARCHAR(50),
+); 
