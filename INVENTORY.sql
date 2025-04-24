3a23103c5f262bfa4a6faa26416ce6f167c9e1ed
IbraheemF <141586321+IbraheemF@users.noreply.github.com>
2025-04-24 13:09:05 -0500

    Update and rename Inventory.sql to INVENTORY.sql

--- INVENTORY.sql
+++ INVENTORY.sql
@@ -2,6 +2,6 @@ CREATE TABLE INVENTORY (
   RThreshold INT, 
   IQuantity INT,
   BookID INT UNIQUE,
-  PRIMARY KEY (BookID, RThreshold, IQuantity,
+  PRIMARY KEY (BookID, RThreshold, IQuantity),
   FOREIGN KEY (BookID) REFERENCES BOOK(BookID)
   ); 
