f212912356273864aa8544c2e6f2af590ba6c37e
djcatlin <152668717+djcatlin@users.noreply.github.com>
2025-04-24 13:02:07 -0500

    Update Inventory.sql

--- Inventory.sql
+++ Inventory.sql
@@ -1,7 +1,7 @@
 CREATE TABLE INVENTORY ( 
   RThreshold INT, 
   IQuantity INT,
-  BookID INT,
+  BookID INT UNIQUE,
   PRIMARY KEY (BookID, RThreshold, IQuantity,
   FOREIGN KEY (BookID) REFERENCES BOOK(BookID)
   ); 
