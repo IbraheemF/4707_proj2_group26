13a08f4b26f5f9862a2a4798211e6c57e0ee7aa1
djcatlin <152668717+djcatlin@users.noreply.github.com>
2025-04-25 00:14:24 -0500

    Update BOOK.sql

--- BOOK.sql
+++ BOOK.sql
@@ -1,9 +1,9 @@
 CREATE TABLE BOOK (
-BookID INT PRIMARY KEY,
-Title VARCHAR(50),
-Genre VARCHAR(50),
-Price FLOAT(4),
-Stock INT,
-PublisherName VARCHAR(50),
-PublisherContactInfo VARCHAR(50)
+  BookID INT PRIMARY KEY,
+  Title VARCHAR(50),
+  Genre VARCHAR(50),
+  Price FLOAT(4),
+  Stock INT,
+  PublisherName VARCHAR(50),
+  PublisherContactInfo VARCHAR(50)
 ); 
