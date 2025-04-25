5d463a0aa2a9f00ecdaa5b53babceb53c8993bb0
djcatlin <152668717+djcatlin@users.noreply.github.com>
2025-04-24 23:55:54 -0500

    Update ORDERS.sql

--- ORDERS.sql
+++ ORDERS.sql
@@ -1,12 +1,12 @@
 CREATE TABLE ORDERS (
-OrderID INT PRIMARY KEY,
-PaymentStatus VARCHAR(50),
-PaymentRecord VARCHAR(50),
-PaymentID INT UNIQUE,
-ShippingDate DATE,
-OrderDate DATE,
-ShippingAddress VARCHAR(50),
-EmployeeID INT UNIQUE,
-FOREIGN KEY (EmployeeID) REFERENCES EMPLOYEE(EmployeeID),
-FOREIGN KEY (PaymentID) REFERENCES PAYMENT(PaymentID),
-); 
+	OrderID INT PRIMARY KEY,
+	PaymentStatus VARCHAR(50),
+	PaymentRecord VARCHAR(50),
+	PaymentID INT,
+	ShippingDate DATE,
+	OrderDate DATE,
+	ShippingAddress VARCHAR(50),
+	EmployeeID INT,
+	FOREIGN KEY (EmployeeID) REFERENCES EMPLOYEE(EmployeeID),
+	FOREIGN KEY (PaymentID) REFERENCES PAYMENT(PaymentID)
+);
